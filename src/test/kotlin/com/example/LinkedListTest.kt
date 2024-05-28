8569bab84a884568f333e7b687e24b09395724e7
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-05-28 18:07:09 -0400

    added real tests

--- src/test/kotlin/com/example/LinkedListTest.kt
+++ src/test/kotlin/com/example/LinkedListTest.kt
@@ -1,11 +1,53 @@
 package com.example
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlin.test.*
+
 
 class LinkedListTest {
+
     @Test
-    fun testFoo() {
-        assertEquals(0, 0)
+    fun `test LinkedList isEmpty`() {
+        val linkedList = mockk<LinkedList<Int>>()
+
+        every { linkedList.isEmpty() } returns true
+
+        assertTrue(linkedList.isEmpty())
+        verify { linkedList.isEmpty() }
+    }
+
+    @Test
+    fun `test LinkedList push and peek`() {
+        val linkedList = mockk<LinkedList<Int>>()
+
+        every { linkedList.add(any()) } answers { }
+        every { linkedList.peek() } returns 42
+
+        linkedList.add(42)
+        assertEquals(42, linkedList.peek())
+
+        verify { linkedList.add(42) }
+        verify { linkedList.peek() }
+    }
+
+    @Test
+    fun `test LinkedList peek throws exception`() {
+        val linkedList = mockk<LinkedList<Int>>()
+
+        val exception = assertFailsWith<Exception> {
+            linkedList.peek()
+        }
+        assertEquals("List is empty", exception.message)
+    }
+
+    @Test
+    fun `test LinkedList delete`() {
+        val linkedList = mockk<LinkedList<Int>>()
+
+        every { linkedList.delete() } returns Unit
+        assertEquals(Unit, linkedList.delete())
+        verify { linkedList.delete() }
     }
 }
